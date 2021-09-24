1c7441f15afe9dd76e7654a0b46905a39c65b9d4
Javier Caicedo <jicaicedos.frontbackend@gmail.com>
2021-09-23 19:56:15 -0500

    login version3

--- modelo/dao/usuario.js
+++ modelo/dao/usuario.js
@@ -9,11 +9,11 @@ Usuarios = []
 
 function iniciar() {
     Usuarios = [
-        {nombre: "Oscar", apellido: "Tascon", cedula: 16379276, usuario: "otascon", password: "1", rol: "Administrador"},
-        {nombre: "Anngie", apellido: "Ospina", cedula: 1053859237, usuario: "aospina", password: "2", rol: "Docente"},
-        {nombre: "Nancy", apellido: "Quintero", cedula: 1053834493, usuario: "nquintero", password: "3", rol: "Docente"},
-        {nombre: "Oscar", apellido: "Fuentes", cedula: 74186115, usuario: "ofuentes", password: "4", rol: "Alumno"},
-        {nombre: "Javier", apellido: "Caicedo", cedula: 83040450, usuario: "jcaicedo",password: "5", rol: "Padre de familia"}
+        {nombre: "Oscar", apellido: "Tascon", cedula: 16379276, usuario: "otascon", password: "11111111", rol: "Administrador"},
+        {nombre: "Anngie", apellido: "Ospina", cedula: 1053859237, usuario: "aospina", password: "222222222", rol: "Docente"},
+        {nombre: "Nancy", apellido: "Quintero", cedula: 1053834493, usuario: "nquintero", password: "33333333", rol: "Docente"},
+        {nombre: "Oscar", apellido: "Fuentes", cedula: 74186115, usuario: "ofuentes", password: "44444444", rol: "Alumno"},
+        {nombre: "Javier", apellido: "Caicedo", cedula: 83040450, usuario: "jcaicedo",password: "55555555", rol: "Padre de familia"}
     ]
 
 }
@@ -29,11 +29,16 @@ function login() {
             alert("La contraseña no debe ser un campo vacío")
         }
         else {
+            let band = false
             Usuarios.forEach(element => {
                 if( element.usuario==usuario && element.password ) {
                     alert("Bienvenido: " + element.nombre + " " + element.apellido)
+                    band = true
                 }
             });
+        }
+        if (band==false) {
+            alert("Usuario no está registrado en el sistema")
         }
     }
 }
